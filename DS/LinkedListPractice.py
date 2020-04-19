636f56bf313c62bae83da719b3cfa3faa2467d85
Anand Benjamin <anands.ben@gmail.com>
2020-04-19 15:17:54 +0530

    Added logic to Position and Count

--- DS/LinkedListPractice.py
+++ DS/LinkedListPractice.py
@@ -3,7 +3,7 @@
 1.Create a List box  ( data,address)
 2.Add elements to List Box
 
-       [1,null]-->[2,null] --> [3,null] 
+       [10,200]-->[20,300] --> [30,400] -->[40,500]-->[50,null]
 
 3.Create a Linked List
 4.Forming a chain
@@ -18,7 +18,6 @@ class Node:
 class LinkedList:
        def __init__(self):
               self.head=None
-
        def appendElement(self,element):
               
               if(self.head==None):
@@ -26,12 +25,10 @@ class LinkedList:
                      print("Head is null so adding new element")
               else:
                      current=self.head                     
-                     
+                     #self.head.nextValue=element
                      while(current.nextValue!=None):
                             current=current.nextValue
-							
                      current.nextValue=element
-
        def printElement(self):
               l=[]
               current=self.head
@@ -40,14 +37,42 @@ class LinkedList:
                      #print(current.data)
                      current=current.nextValue
               return l
+       def countofList(self):
+              current=self.head
+              count=0
+              while(current!=None):
+                     count+=1
+                     current=current.nextValue
+              return count
+       def positionValue(self,userposition):
+              current=self.head
+              position=1
+              print("size",self.countofList())
+              
+              if(self.countofList()>=userposition):
                      
+                     while(userposition!=position):
+                            current=current.nextValue
+                            position+=1
+                     return current.data
+              else:
+                     print("position is greater than list size")
+                     return -1
+              
+       
 obj1=Node(1)  #object created and value passed
 print(obj1)
 obj2=Node(2)
 print(obj2)
 obj3=Node(3)
 #print(obj3)
+"""
+obj1.nextValue=obj2
+print(obj1.nextValue)
+obj2.nextValue=obj3
 
+print(obj2.nextValue)
+"""
 Llist=LinkedList()
 #print(Llist)
 Llist.appendElement(obj1)
@@ -67,3 +92,5 @@ print(Llist.printElement())
 print(obj1.nextValue.data)
 #obj2.nextValue=obj3
 print(obj2.nextValue.data)
+print(Llist.countofList())
+print(Llist.positionValue(2))
